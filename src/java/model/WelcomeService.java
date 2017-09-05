fb090f59817f07d4b548e5c61e1828466b193f57
Nolan <Nolan@10.30.23.64>
2017-09-05 15:00:30 -0500

    third commit - everything is working *correct assignment completed*

--- src/java/model/WelcomeService.java
+++ src/java/model/WelcomeService.java
@@ -7,6 +7,7 @@ package model;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  *
@@ -14,16 +15,33 @@ import java.util.Calendar;
  */
 public class WelcomeService {
 
-    public String produceMessage(String birthday) {
+    public String produceMessage(String name) {
+        String welcomeMsg = "";
+        String hourString;
+        int hour = 0; 
+        
         Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DATE, 0);
-        SimpleDateFormat format1 = new SimpleDateFormat("MM/dd/yyyy");
-
-        String formatted = format1.format(cal.getTime());
-        if (formatted == null ? birthday == null : formatted.equals(birthday)) {
-            return "Happy Birthday!";
+        SimpleDateFormat sdf = new SimpleDateFormat("HH");
+        hourString = sdf.format(cal.getTime());
+        hour = Integer.parseInt(hourString);
+        
+        if (hour <= 12) {
+        welcomeMsg = "Good Morning";
+        } else if (hour <= 16){
+            welcomeMsg = "Good Afternoon";
+        } else {
+            welcomeMsg = "Good Night";
         }
-        return "Today is not your birthday.";
-    }
+      return welcomeMsg + " " + name;
+}
 
+public String determineTimeOfDay(String date) {
+        String TimeOfDay = "No Time of Day Determined";
+        return TimeOfDay;
+    }
+    
+    public String combineNameMessage(String name, String TimeOfDay){
+        String welcomeMessage = TimeOfDay +" " + name;
+        return welcomeMessage;       
+    }
 }
